04b2aa5869e973c05d1b8c970e9c131f3483952d
egueguen <evan.gueguen@etudiant.univ-lr.fr>
2023-12-16 12:49:42 +0100

    Debug

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react"
 
 const Form = () => {
+
+  const [value, setValue] = useState('')
+
+  const handleClick = evt => {
+    setValue(evt.target.value)
+  }
+
     return<form>
     <input
         className="new-todo"
         placeholder="Qu'avez vous à faire ?"
+        onChange={ handleClick() }
+        value={ value }
         autoFocus
     />
-    <input className="hidden" type="submit" value="Ajouter" />
+    <input className="hidden" type="submit" value="Ajouter"/>
   </form>
 }
 
-export default Form;
+export default Form
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"
 
 const TodoItem = props => {
 
@@ -18,24 +18,6 @@ const TodoItem = props => {
           }
     ]
 
-    // const {
-    //     name,
-    //     complete,
-    //     id
-    // } = props
-
-//     return<li className="completed">
-//     <div className="view">
-//       <input className="toggle" type="checkbox" checked={ complete } id={ id }/>
-//       <label>{ name }</label>
-//       <button className="destroy" />
-//     </div>
-//     <form>
-//       <input className="edit" defaultValue={ name } />
-//       <input type="submit" value="Valider" className="hidden" />
-//     </form>
-//   </li>
-
     return<ul>
         {todoList.map(todo => <TodoItem 
             id={todo.id}
@@ -46,4 +28,4 @@ const TodoItem = props => {
     </ul>
 }
 
-export default TodoItem;
+export default TodoItem
