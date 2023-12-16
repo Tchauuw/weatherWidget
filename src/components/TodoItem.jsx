8217e2ad76b65fe07da57d39f86b0dc6dbadcd3f
egueguen <evan.gueguen@etudiant.univ-lr.fr>
2023-12-16 10:46:35 +0100

    Ex 3 : Iterating on tasks

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -2,23 +2,48 @@ import React from "react";
 
 const TodoItem = props => {
 
-    const {
-        name,
-        complete,
-        id
-    } = props
+    const todoList = [
+        {  
+            id: 'todo-1',  
+            name: 'Taper un tacos 5 viandes',  
+            complete: true}, 
+          {  
+            id: 'todo-2',  
+            name: 'Battre Mimie MATHY à la bagarre',  
+            complete: false}, 
+          {  
+            id: 'todo-3',  
+            name: 'Boire de la water avec JCVD',  
+            complete: false
+          }
+    ]
 
-    return<li className="completed">
-    <div className="view">
-      <input className="toggle" type="checkbox" checked={ complete } id={ id }/>
-      <label>{ name }</label>
-      <button className="destroy" />
-    </div>
-    <form>
-      <input className="edit" defaultValue={ name } />
-      <input type="submit" value="Valider" className="hidden" />
-    </form>
-  </li>
+    // const {
+    //     name,
+    //     complete,
+    //     id
+    // } = props
+
+//     return<li className="completed">
+//     <div className="view">
+//       <input className="toggle" type="checkbox" checked={ complete } id={ id }/>
+//       <label>{ name }</label>
+//       <button className="destroy" />
+//     </div>
+//     <form>
+//       <input className="edit" defaultValue={ name } />
+//       <input type="submit" value="Valider" className="hidden" />
+//     </form>
+//   </li>
+
+    return<ul>
+        {todoList.map(todo => <TodoItem 
+            id={todo.id}
+            name={todo.name}
+            complete={todo.complete}
+            key={todo.id}
+        />)}
+    </ul>
 }
 
 export default TodoItem;
